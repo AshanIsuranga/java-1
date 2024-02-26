52a7e950e1415d892270f54c30cd99b5704102ad
Ashanisuranga <uaa.isuranga@gmail.com>
2024-02-26 14:35:48 +0530

    2nd tag commit

---
+++ MethodOverrideExample.java
@@ -0,0 +1,26 @@
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+/*
+ * we can override concrete methods in subclasses by extending the super class
+ * which have the method
+ */
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class MethodOverrideExample {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.makeSound(); // Output: Animal makes a sound
+
+        Dog dog = new Dog();
+        dog.makeSound(); // Output: Dog barks
+    }
+}
